1bb9a7e1b7cbb72792e07f789593a1e1bcf29d09
Cemal Demir <CMLDMR@users.noreply.github.com>
2018-09-06 21:03:07 +0300

    new: define MACROS for detecting platform is ios or mac

--- qmongodb.h
+++ qmongodb.h
@@ -14,13 +14,45 @@
 #include <QMimeDatabase>
 
 
+#ifdef Q_OS_WIN // Q_OS_IOS
+#define MAC_IOS
+#endif
+
+#ifdef Q_OS_MACOS
+#define MAC_IOS
+#endif
+
+#ifdef Q_OS_TVOS
+#define MAC_IOS
+#endif
+
+#ifdef Q_OS_WATCHOS
+#define MAC_IOS
+#endif
+
+#ifdef MAC_IOS
+#include <QTcpSocket>
+#else
+
+#endif
+
+#ifdef MAC_IOS
+
+#else
+
+#endif
+
 
 class QMONGODBSHARED_EXPORT QMongoDB : public QObject
 {
     Q_OBJECT
 public:
 
+#ifdef MAC_IOS
+    QMongoDB(QString serverip , QString database , QObject* parent = nullptr);
+#else
     QMongoDB(QString mongodburl , QString database , QObject* parent = nullptr);
+#endif
 
     ~QMongoDB();
 
@@ -67,10 +99,19 @@ Q_SIGNALS:
     void gridfsbytereceived(int);
 
 private:
+
+
+#ifdef MAC_IOS
+
+    QTcpSocket* mSocket;
+
+#else
     QString mUrl;
     QString db;
-
     QString mLastError;
+#endif
+
+
 
 
 
