79317466cccab8990194133407035e605a566ffd
Cemal Demir <CMLDMR@users.noreply.github.com>
2018-09-12 16:04:02 +0300

    add: QCORE_APPLICATION_STARTUP added

--- qml/qmlmongodb.cpp
+++ qml/qmlmongodb.cpp
@@ -1,4 +1,18 @@
 #include "qmlmongodb.h"
+#include <QtQml/QQmlApplicationEngine>
+
+#define MAJOR   0
+#define MINOR   5
+
+
+static void registerQmlMongoTypes() {
+    qmlRegisterType<QMLMongoDB>("com.mongodb", MAJOR, MINOR, "Mongo");
+}
+
+Q_COREAPP_STARTUP_FUNCTION(registerQmlMongoTypes)
+
+
+
 
 QMLMongoDB::QMLMongoDB(QString mongourl, QString database)
     : QMongoDB(mongourl,database)
